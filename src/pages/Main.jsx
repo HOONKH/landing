a369a77f9d5d18dd13a9b8d463180f9f8a278688
HOONKH <boospa0305@gmai.com>
2023-11-25 22:46:45 +0900

    quote

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,28 @@
 import { Link } from "react-router-dom";
 import Video from "../components/Video";
+import "animate.css";
 
 const main = () => {
+  const date = new Date();
+  const endDate = new Date("2024-02-29");
+  const dDay = date - endDate;
+  const setOurDday = Math.floor(dDay / (1000 * 60 * 60 * 24));
+
   return (
-    <div className="min-h-screen text-white flex justify-center items-center flex-col gap-5">
+    <div className="min-h-screen text-white flex justify-center items-center flex-col gap-5 whitespace-pre-wrap">
       <Video />
-      HI THERE
+      {`When everything seems to 
+    be going against you,
+      remember that the
+  airplane takes off against 
+     the wind, not with it 
+             -H662`}
+      <span className="animate__animated animate__zoomIn text-3xl">{`블체스 화이팅!! D${setOurDday} `}</span>
       <div className="flex gap-5">
-        <button className="border-2 border-none px-2 bg-[#3F833F] hover:bg-green-400">
+        <button className="border-2 border-none px-2 bg-[#3F833F] hover:bg-green-400 animate__animated animate__slideInUp">
           <Link to="/benjerry">BEN&JERRY</Link>
         </button>
-        <button className="border-2 border-none px-2 text-black bg-[#fff200] shadow-2xl shadow-[#3f833f] hover:bg-yellow-100">
+        <button className="border-2 border-none px-2 text-black bg-[#fff200] shadow-2xl shadow-[#3f833f] hover:bg-yellow-100 animate__animated animate__slideInUp">
           <Link to="/wafflehouse">WAFFLEHOUSE</Link>
         </button>
       </div>
